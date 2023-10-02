1d1c4033ec090f8b87788f7415e541025005329a
Morgan <morgan.matprof@gmail.com>
2023-10-02 18:02:24 -0300

    changes to quality system

--- DiabloMod.cs
+++ DiabloMod.cs
@@ -1,5 +1,6 @@
 using Terraria.ModLoader;
 using DiabloMod.Items;
+using IL.Terraria;
 
 namespace DiabloMod
 {
@@ -12,15 +13,9 @@ namespace DiabloMod
         {
             GlobalItem globalItem = new Weapon();
             GlobalItem qualityGlobal = new Quality();
-
-            // Getting modded item ID to test quality system
-            moddedItemID = ModContent.ItemType<Items.ShortSword>();
-
-            // Setting modded item ID to quality data structure, so that we can assign it's ID to the designed quality value
-            ItemQualityData.Instance.SetQuality(moddedItemID, qualityValue);
+            
 
         }
-
 
     }
 }
--- Quality.cs
+++ Quality.cs
@@ -4,6 +4,7 @@ using Humanizer;
 using Terraria;
 using Terraria.ModLoader;
 using DiabloMod.Items;
+using Microsoft.Xna.Framework;
 
 namespace DiabloMod {
 
@@ -14,12 +15,37 @@ namespace DiabloMod {
         public override bool InstancePerEntity => true;
 
         // update tooltip with item quality
+
+        /*
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
             if(ItemQualityData.Instance.ItemQualityMap.ContainsKey(item.type)) {
                 float quality = ItemQualityData.Instance.ItemQualityMap[item.type];
                 tooltips.Add(new TooltipLine(Mod, "Quality", $"Quality: {quality * 100}%"));
             }
+        }
+        */
+
+        // updated version of tooltip method
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if(item.damage > 0 || item.defense > 0) {
+                if (ItemQualityData.Instance.TryGetQuality(item.type, out float quality)) {
+                    // calculates new damage value based on quality
+                    item.damage = (int)(item.damage * (quality * 2));
+
+                    // calculates new armor value based on quality
+                    if (item.defense > 0) {
+                        item.defense = (int)(item.defense * (quality * 0.5));
+                    }
+
+                    TooltipLine qualityTooltip = new TooltipLine(Mod, "Quality",$"Quality: {quality * 100}%");
+                    qualityTooltip.OverrideColor = new Color(255, 255, 0);
+                    tooltips.Add(qualityTooltip);
+
+                }
+            }
+
         }
 
         public void QualityRandomizer(Item item) {
@@ -29,7 +55,7 @@ namespace DiabloMod {
             // quality, obviously
             float quality;
             // checks if item has damage and if there is quality associated
-            if (item.damage > 0 && ItemQualityData.Instance.TryGetQuality(item.type, out quality)) {
+            if (((item.damage > 0 || item.defense > 0)) && (ItemQualityData.Instance.TryGetQuality(item.type, out quality))) {
                 float randomValue = Main.rand.NextFloat();
                 float randomQuality;
                 if (randomValue < lowQuality) {
